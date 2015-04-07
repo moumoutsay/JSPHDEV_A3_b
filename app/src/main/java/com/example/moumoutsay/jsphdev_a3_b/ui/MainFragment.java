c176c864b75717e798cc22a3799a5a79e6402d7e
moumoutsay <weilints@andrew.cmu.edu>
2015-04-07 15:32:38 -0700

    Init refactor

---
+++ app/src/main/java/com/example/moumoutsay/jsphdev_a3_b/ui/MainFragment.java
@@ -0,0 +1,25 @@
+package com.example.moumoutsay.jsphdev_a3_b.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.moumoutsay.jsphdev_a3_b.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
