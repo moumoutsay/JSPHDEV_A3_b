8c31de4a495155d09404b3479585b3221055d0d3
moumoutsay <weilints@andrew.cmu.edu>
2015-04-07 19:52:00 -0700

    Activity switch ok

--- app/src/main/java/com/example/moumoutsay/jsphdev_a3_b/ui/ContentFragment.java
+++ app/src/main/java/com/example/moumoutsay/jsphdev_a3_b/ui/ContentFragment.java
@@ -1,17 +1,19 @@
 package com.example.moumoutsay.jsphdev_a3_b.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.moumoutsay.jsphdev_a3_b.R;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ContentFragment extends Fragment {
+public class ContentFragment extends Fragment implements View.OnClickListener {
 
     public ContentFragment() {
     }
@@ -20,6 +22,35 @@ public class ContentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_content, container, false);
+
+        Button buttonSong = (Button) rootView.findViewById(R.id.buttonSong);
+        Button buttonVideo = (Button) rootView.findViewById(R.id.buttonVideo);
+        Button buttonWallPaper = (Button) rootView.findViewById(R.id.buttonWallPaper);
+        Button buttonGetOnMailList = (Button) rootView.findViewById(R.id.buttonGotOnMail);
+
+        buttonSong.setOnClickListener(this);
+        buttonVideo.setOnClickListener(this);
+        buttonWallPaper.setOnClickListener(this);
+        buttonGetOnMailList.setOnClickListener(this);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonGotOnMail:
+                // TODO
+                break;
+            case R.id.buttonSong:
+                startActivity(new Intent(getActivity(),SongActivity.class));
+                break;
+            case R.id.buttonVideo:
+                startActivity(new Intent(getActivity(),VideoActivity.class));
+                break;
+            case R.id.buttonWallPaper:
+                startActivity(new Intent(getActivity(),WallPaperActivity.class));
+                break;
+        }
     }
 }
